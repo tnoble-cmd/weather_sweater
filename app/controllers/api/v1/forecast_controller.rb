71802a9dbb803952467c852bf3ba552275f444f5
Tyler Noble <tmnoble11@gmail.com>
2024-09-22 16:22:02 -0600

    tried live request with postman and was getting Tai-Wan.. pryed my way through it to find i was passing wrong params coordinates[:lon] instead of [:lng]. Working now.

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::ForecastController < ApplicationController
   def show
     coordinates = MapQuestService.new(params[:location]).get_coordinates
-    weather_data = WeatherService.new(coordinates[:lat], coordinates[:lon]).get_weather
-
+    
+    weather_data = WeatherService.new(coordinates[:lat], coordinates[:lng]).get_weather
     render json: ForecastSerializer.new(weather_data).serializable_hash.to_json
   end
 end
