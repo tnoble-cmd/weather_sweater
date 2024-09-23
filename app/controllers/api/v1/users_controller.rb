cb2e91dd66be2c6e177a8b262f339bf3b9acb2af
Tyler Noble <tmnoble11@gmail.com>
2024-09-23 08:27:44 -0600

    US 2: aded route for user registration, Created: usersController.

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+  
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
+end
